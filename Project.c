e71e9808606e7515897fa3902fdc7b9dad4ea81b
Goldy-2007 <goldykumarihtce@gmail.com>
2025-12-22 13:20:09 +0530

    Push my first project

---
+++ Project.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+    int choice;
+    printf("********BUS RESERVATION SYSTEM********\n");
+    printf("1. Login \n");
+    printf("2. Exit \n");
+    printf("Please enter your choice:");
+    scanf("%d",&choice);
+    
+      return 0;
+}
